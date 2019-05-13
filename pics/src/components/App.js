97bf5f17492d93fed5edda4307c50b4e873d427f
James Bouy <jbouy@turner-industries.com>
2019-05-13 08:08:48 -0500

    s10l106: dynamic spans

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,6 +3,7 @@ import { Container } from "semantic-ui-react";
 import unsplash from "../api/unsplash";
 
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends Component {
   state = { images: [] };
@@ -19,7 +20,7 @@ class App extends Component {
     return (
       <Container style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </Container>
     );
   }
