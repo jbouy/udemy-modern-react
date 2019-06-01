cba2732851434773451c41f8baf6834196fca003
James Bouy <jbouy@turner-industries.com>
2019-06-01 09:59:02 -0500

    initalize `seasons-hook` application

---
+++ seasons-hooks/src/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Spinner from "./Spinner";
+import SeasonDisplay from "./SeasonDisplay";
+
+class App extends React.Component {
+  state = {
+    lat: null,
+    errorMessage: ""
+  };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  renderContent() {
+    const { errorMessage, lat } = this.state;
+
+    if (errorMessage && !lat) {
+      return <div>Error: {errorMessage}</div>;
+    }
+
+    if (!errorMessage && lat) {
+      return <SeasonDisplay lat={lat} />;
+    }
+
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
